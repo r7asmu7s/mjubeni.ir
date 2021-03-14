243b5af2e22766ecf910238e7802ebbbdfd9f89c
Mehrzad M <r7asmu7s@gmail.com>
2021-03-14 21:14:13 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,28 +1,19 @@
-const days = document.querySelector('#days')
-const hours = document.querySelector('#hours')
-const minutes = document.querySelector('#minutes')
-const seconds = document.querySelector('#seconds')
-const countdown = document.querySelector('#countdown')
-const uploadDate = new Date(`April 05 2021 12:00:00`)
+const textEl = document.getElementById('text')
+const text = 'I am an English Teacher.'
+let idx = 1
 
-// Update countdown time
-function updateCountdown() {
-	const currentTime = new Date()
-	const diff = uploadDate - currentTime
+writeText()
 
-	const d = Math.floor(diff / 1000 / 60 / 60 / 24)
-	const h = Math.floor(diff / 1000 / 60 / 60) % 24
-	const m = Math.floor(diff / 1000 / 60) % 60
-	const s = Math.floor(diff / 1000) % 60
+function writeText() {
+	textEl.innerText = text.slice(0, idx)
 
-	// Add values to DOM
-	days.innerHTML = d
-	hours.innerHTML = h < 10 ? '0' + h : h
-	minutes.innerHTML = m < 10 ? '0' + m : m
-	seconds.innerHTML = s < 10 ? '0' + s : s
+	idx++
+
+	if (idx > text.length) {
+		idx = 1
+	}
+
+	setTimeout(writeText, 200)
 }
-
-setInterval(updateCountdown, 1000)
-
 // Get the current year for the copyright
 $('#year').text(new Date().getFullYear())
